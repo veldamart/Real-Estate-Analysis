d1de8924a9ff5cd991f5dcc2315515f4815f9c17
Velda <veldamart@gmail.com>
2020-04-07 15:41:34 -0400

    committ all

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Real_Estate_Analysis
+{
+    class Program
+    {
+        static void openfile()
+        {
+          /*this will open and excel file*/
+            string path ="C:/Users/Velda/Desktop/Real Estate Analysis/Data/Mortgage payment calculator.xlsx";
+            var excelApp = new Application();
+            excelApp.Visible = true;
+
+            Excel.Workbooks books = excelApp.Workbooks;
+
+            Excel.Workbook sheet = books.Open(path);
+        }
+    
+
+        static void Main(string[] args)
+        {
+            openfile();
+        }
+    }
+}
